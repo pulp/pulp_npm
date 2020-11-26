a4d25b8b40e74b74ef1ce43120515290612a9969
Fabricio Aguiar <fabricio.aguiar@gmail.com>
2020-11-26 10:19:44 -0300

    Fix docs script
    [noissue]

--- .ci/scripts/publish_docs.sh
+++ .ci/scripts/publish_docs.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # WARNING: DO NOT EDIT!
 #
@@ -9,22 +9,29 @@
 
 set -euv
 
-# make sure this script runs at the repo root
-cd "$(dirname "$(realpath -e "$0")")"/../..
+export WORKSPACE=$GITHUB_WORKSPACE
+export BRANCH=$GITHUB_REF_NAME_SLUG
 
-openssl aes-256-cbc -K $encrypted_None_key -iv $encrypted_None_iv -in .ci/assets/pulp-infra.enc -out ~/.ssh/pulp-infra -d
-sudo chmod 600 ~/.ssh/pulp-infra
+mkdir ~/.ssh
+echo "$PULP_DOCS_KEY" > ~/.ssh/pulp-infra
+chmod 600 ~/.ssh/pulp-infra
 
-echo "docs.pulpproject.org,8.43.85.236 ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBGXG+8vjSQvnAkq33i0XWgpSrbco3rRqNZr0SfVeiqFI7RN/VznwXMioDDhc+hQtgVhd6TYBOrV07IMcKj+FAzg=" >> /home/runner/.ssh/known_hosts
-chmod 644 /home/runner/.ssh/known_hosts
+echo "docs.pulpproject.org,8.43.85.236 ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBGXG+8vjSQvnAkq33i0XWgpSrbco3rRqNZr0SfVeiqFI7RN/VznwXMioDDhc+hQtgVhd6TYBOrV07IMcKj+FAzg=" >> ~/.ssh/known_hosts
+chmod 644 ~/.ssh/known_hosts
 
 pip3 install -r doc_requirements.txt
 
 export PYTHONUNBUFFERED=1
 export DJANGO_SETTINGS_MODULE=pulpcore.app.settings
-export PULP_SETTINGS=$PWD/.ci/ansible/settings/settings.py
+export PULP_SETTINGS=$WORKSPACE/.github/settings/settings.py
 
 eval "$(ssh-agent -s)" #start the ssh agent
 ssh-add ~/.ssh/pulp-infra
 
-python3 .ci/scripts/docs-builder.py --build-type $1 --branch $2
+if [[ $GITHUB_REF = "refs/heads"* ]]; then
+  type="nightly"
+else
+  type="tag"
+fi
+
+python3 $WORKSPACE/.ci/scripts/docs-builder.py --build-type $type --branch $BRANCH

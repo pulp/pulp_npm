72d6622a31f5b753fee1d7dc46f827381de2a9f3
David Davis <daviddavis@redhat.com>
2020-11-30 12:30:57 -0500

    Update github files
    [noissue]

--- .ci/scripts/publish_docs.sh
+++ .ci/scripts/publish_docs.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 # WARNING: DO NOT EDIT!
 #
@@ -9,29 +9,23 @@
 
 set -euv
 
-export WORKSPACE=$GITHUB_WORKSPACE
-export BRANCH=$GITHUB_REF_NAME_SLUG
+# make sure this script runs at the repo root
+cd "$(dirname "$(realpath -e "$0")")"/../..
 
 mkdir ~/.ssh
 echo "$PULP_DOCS_KEY" > ~/.ssh/pulp-infra
 chmod 600 ~/.ssh/pulp-infra
 
-echo "docs.pulpproject.org,8.43.85.236 ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBGXG+8vjSQvnAkq33i0XWgpSrbco3rRqNZr0SfVeiqFI7RN/VznwXMioDDhc+hQtgVhd6TYBOrV07IMcKj+FAzg=" >> ~/.ssh/known_hosts
-chmod 644 ~/.ssh/known_hosts
+echo "docs.pulpproject.org,8.43.85.236 ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBGXG+8vjSQvnAkq33i0XWgpSrbco3rRqNZr0SfVeiqFI7RN/VznwXMioDDhc+hQtgVhd6TYBOrV07IMcKj+FAzg=" >> /home/runner/.ssh/known_hosts
+chmod 644 /home/runner/.ssh/known_hosts
 
 pip3 install -r doc_requirements.txt
 
 export PYTHONUNBUFFERED=1
 export DJANGO_SETTINGS_MODULE=pulpcore.app.settings
-export PULP_SETTINGS=$WORKSPACE/.github/settings/settings.py
+export PULP_SETTINGS=$PWD/.ci/ansible/settings/settings.py
 
 eval "$(ssh-agent -s)" #start the ssh agent
 ssh-add ~/.ssh/pulp-infra
 
-if [[ $GITHUB_REF = "refs/heads"* ]]; then
-  type="nightly"
-else
-  type="tag"
-fi
-
-python3 $WORKSPACE/.ci/scripts/docs-builder.py --build-type $type --branch $BRANCH
+python3 .ci/scripts/docs-builder.py --build-type $1 --branch $2

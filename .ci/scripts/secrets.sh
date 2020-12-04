8b3419f12db70f3fa7569f72be1ee046d9711c16
Fabricio Aguiar <fabricio.aguiar@gmail.com>
2020-12-04 15:43:55 -0300

    Setting secrets
    ref: https://github.community/t/exporting-all-secrets-in-a-workflow-action/122365/7
    
    [noissue]

---
+++ .ci/scripts/secrets.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Create .env file from GitHub secrets
+
+json_secrets_str=$1
+env_filename=".env"
+
+jq -r 'to_entries|map("\(.key)=\(.value|tostring)")|.[]' <<< "$json_secrets_str" > "$env_filename"
+cat .env >> $GITHUB_ENV

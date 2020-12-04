ba35b8c770d87228a514094cd1837688fc43bf7f
Fabricio Aguiar <fabricio.aguiar@gmail.com>
2020-12-04 19:01:45 -0300

    Set nightly back to once a day
    finishing tests
    [noissue]

--- .ci/scripts/secrets.py
+++
@@ -1,9 +0,0 @@
-import json
-import sys
-
-secrets = json.loads(sys.argv[1])
-print(secrets.keys())
-
-with open(".env", "w") as dotenv:
-    for key, value in secrets.items():
-        dotenv.write(f"{key}={value}")

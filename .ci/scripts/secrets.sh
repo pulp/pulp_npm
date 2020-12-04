c92fd0f65eaed86a69164b9e923dd087dd290fd1
Fabricio Aguiar <fabricio.aguiar@gmail.com>
2020-12-04 17:45:14 -0300

    Update secrets script
    [noissue]

--- .ci/scripts/secrets.sh
+++ .ci/scripts/secrets.sh
@@ -2,8 +2,9 @@
 # Create .env file from GitHub secrets
 set -mveuo pipefail
 
-json_secrets_str=$1
+echo $1 >> secrets.json
+cat secrets.json
 env_filename=".env"
 
-jq -r 'to_entries|map("\(.key)=\(.value|tostring)")|.[]' <<< "$json_secrets_str" > "$env_filename"
+jq -r 'to_entries|map("\(.key)=\(.value|tostring)")|.[]' <<< `cat secrets.json` > "$env_filename"
 cat .env >> $GITHUB_ENV

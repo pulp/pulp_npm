0f28c82213eae3e6fccdb6b3ab77bea94c8ef0a3
Fabricio Aguiar <fabricio.aguiar@gmail.com>
2020-12-05 00:32:25 -0300

    Update secrets script
    [noissue]

--- .ci/scripts/secrets.py
+++ .ci/scripts/secrets.py
@@ -1,8 +1,10 @@
 import json
-import os
 import sys
 
 secrets = json.loads(sys.argv[1])
-for key, value in secrets.items():
-    print(f"Setting {key} ...")
-    os.system(f'echo "{key}={value}" >> $GITHUB_ENV')
+dotenv_path = sys.argv[2]
+
+with open(dotenv_path, "a") as dotenv:
+    for key, value in secrets.items():
+        print(f"Setting {key} ...")
+        dotenv.write(f"{key}={value}")

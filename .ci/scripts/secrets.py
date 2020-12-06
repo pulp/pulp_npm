c3fe2084adef3e31df3bf80ff91a94d52ccdf62f
Fabricio Aguiar <fabricio.aguiar@gmail.com>
2020-12-06 12:01:16 -0300

    Secrets script without PULP_DOCS_KEY
    [noissue]

--- .ci/scripts/secrets.py
+++ .ci/scripts/secrets.py
@@ -6,5 +6,9 @@ dotenv_path = sys.argv[2]
 
 with open(dotenv_path, "a") as dotenv:
     for key, value in secrets.items():
+        if key == "PULP_DOCS_KEY":
+            # Test if the error only happens with PULP_DOCS_KEY
+            print("Skipping PULP_DOCS_KEY")
+            continue
         print(f"Setting {key} ...")
         dotenv.write(f"{key}={value}")

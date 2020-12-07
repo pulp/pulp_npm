b37315356c124d9fc3bd81bef4a4706c37ffb146
Fabricio Aguiar <fabricio.aguiar@gmail.com>
2020-12-07 10:12:07 -0300

    Count env var lines
    [noissue]

--- .ci/scripts/secrets.py
+++ .ci/scripts/secrets.py
@@ -8,7 +8,8 @@ with open(dotenv_path, "a") as dotenv:
     for key, value in secrets.items():
         if key == "PULP_DOCS_KEY":
             # Test if the error only happens with PULP_DOCS_KEY
-            print("Skipping PULP_DOCS_KEY")
+            lines = len(value.split("\n"))
+            print(f"Skipping PULP_DOCS_KEY - Lines {lines}")
             continue
         print(f"Setting {key} ...")
         dotenv.write(f"{key}={value}\n")

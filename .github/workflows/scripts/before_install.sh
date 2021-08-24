cc1e754dc7710ee4d6f4d48082d2feaaf97c6061
pulpbot <pulp-infra@redhat.com>
2021-08-24 02:42:21 +0000

    Update CI files
    [noissue]

--- .github/workflows/scripts/before_install.sh
+++ .github/workflows/scripts/before_install.sh
@@ -107,6 +107,7 @@ fi
 git clone --depth=1 https://github.com/pulp/pulpcore.git --branch master
 
 cd pulpcore
+
 if [ -n "$PULPCORE_PR_NUMBER" ]; then
   git fetch --depth=1 origin pull/$PULPCORE_PR_NUMBER/head:$PULPCORE_PR_NUMBER
   git checkout $PULPCORE_PR_NUMBER

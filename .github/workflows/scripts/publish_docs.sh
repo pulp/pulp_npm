18c58b4cf10c60862e67140afcfbb52ca1fe50f6
pulpbot <pulp-infra@redhat.com>
2022-03-25 12:38:17 +0000

    Update CI files
    [noissue]

--- .github/workflows/scripts/publish_docs.sh
+++ .github/workflows/scripts/publish_docs.sh
@@ -37,7 +37,7 @@ if [[ "$GITHUB_WORKFLOW" == "Npm changelog update" ]]; then
   exit
 fi
 
-pip install mkdocs pymdown-extensions
+pip install mkdocs pymdown-extensions "Jinja2<3.1"
 
 mkdir -p ../bindings
 tar -xvf python-client-docs.tar --directory ../bindings
@@ -60,4 +60,4 @@ mkdocs build
 rsync -avzh site/ doc_builder_pulp_npm@docs.pulpproject.org:/var/www/docs.pulpproject.org/pulp_npm_client/
 
 # publish to docs.pulpproject.org/pulp_npm_client/en/{release}
-rsync -avzh site/ doc_builder_pulp_npm@docs.pulpproject.org:/var/www/docs.pulpproject.org/pulp_npm_client/en/"$1"
+rsync -avzh site/ doc_builder_pulp_npm@docs.pulpproject.org:/var/www/docs.pulpproject.org/pulp_npm_client/en/"$2"

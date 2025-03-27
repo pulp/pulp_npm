9b1013c8d9c9adf521eb69667bfd186ecccc9f90
pulpbot <pulp-infra@redhat.com>
2025-03-27 19:53:57 +0000

    Release 0.3.1

--- pulp_npm/app/__init__.py
+++ pulp_npm/app/__init__.py
@@ -6,6 +6,6 @@ class PulpNpmPluginAppConfig(PulpPluginAppConfig):
 
     name = "pulp_npm.app"
     label = "npm"
-    version = "0.3.1.dev"
+    version = "0.3.1"
     python_package_name = "pulp-npm"
     domain_compatible = True
